a993a5c1e10e51fb2aacfd636d8c7d1f3774138c
João Pedro Lopes <67436337+joaopedro10@users.noreply.github.com>
2023-05-29 20:56:20 -0300

    Development of dynamic pop-ups and improved design of cards

--- Primeiro site/scripts.js
+++ Primeiro site/scripts.js
@@ -4,10 +4,13 @@ const cards = document.querySelectorAll ('.card'); // variável que procura e gu
 
 for (let card of cards){
     card.addEventListener ("click",function(){
+        const videoId = card.getAttribute ("id")
         modal_overlay.classList.add('active')
+        modal_overlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
     })
 }
 
 document.querySelector ('.close_modal').addEventListener("click",function(){
     modal_overlay.classList.remove("active")
+    modal_overlay.querySelector("iframe").src = ""
 })
